17847bb407a8e69d8d7c180248d52a66e3370008
codewithgurjas <gurjas.singh@softobiz.com>
2022-11-11 19:18:08 +0530

    Express generator and MVC Implemented

---
+++ models/db.js
@@ -0,0 +1,12 @@
+// Import the mongoose module
+// const mongoose = require("mongoose");
+
+// // Set up default mongoose connection
+// const mongoDB = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.4kfw6ng.mongodb.net/?retryWrites=true&w=majority";
+// mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// // Get the default connection
+// const db = mongoose.connection;
+
+// // Bind connection to error event (to get notification of connection errors)
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
